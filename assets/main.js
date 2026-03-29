4e359763ab3a70d490ed8657f4e65fb52b40db3c
miatran27 <tranm622@newschool.edu>
2026-03-29 17:14:55 -0400

    publish

---
+++ assets/main.js
@@ -0,0 +1,8 @@
+fetch('assets/data.json')
+	.then(response => response.json())
+	.then(data => {
+		// And passes the data to the function, above!
+		// renderItems(data)
+        console.log(data)
+	})
+	.catch(error => console.error('Error loading data:', error))
