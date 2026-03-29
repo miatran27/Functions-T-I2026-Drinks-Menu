cdca447284867f16385d2d61a12fde957e08e6fc
miatran27 <tranm622@newschool.edu>
2026-03-29 18:07:05 -0400

    added some mood buttons in

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,23 @@
+let renderItems = (data) => {
+    let containerEl = document.querySelector('#drink-name')
+    data.forEach(item => {
+        let itemHtml = `
+        <li>
+            <h2>${item['Mood']}</h2>
+            <p>${item['Drink name']}</p>
+        </li>
+        `
+
+        containerEl.insertAdjacentHTML('beforeend', itemHtml)
+    })
+}
+
 fetch('assets/data.json')
 	.then(response => response.json())
 	.then(data => {
 		// And passes the data to the function, above!
 		// renderItems(data)
-        console.log(data)
+        renderItems (data)
 	})
-	// .catch(error => console.error('Error loading data:', error))
+    
+
